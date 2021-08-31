4a02c81ae75f3ced00d08a5b8c1584f427e94c18
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-08-31 10:42:21 +0300

    Further refactor

--- compress_images.py
+++ compress_images.py
@@ -5,8 +5,20 @@ import os
 import subprocess
 import shutil
 
-DEFAULT_QUQLITY = 20
+DEFAULT_QUALITY = 20
 SUFFIX = '.CMPRSIMG'
+
+class ImageInfo(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file_size = path.getsize(file_path)
+        w, h = get_image_size(file_path)
+        self.image_size = [w, h]
+
+    def __str__(self):
+        return '{} ({}K, {}x{})'.format(
+            self.file_path, round(self.file_size / 1024), self.image_size[0], self.image_size[1]
+        )
 
 def compress_image(file_path, quality, is_inplace=False, out_dir=None):
     src_file = path.abspath(file_path)
@@ -17,31 +29,24 @@ def compress_image(file_path, quality, is_inplace=False, out_dir=None):
     try:
         subprocess.run(
             ['convert', '-quality', str(quality) + '%', src_file, interim_file],
-            capture_output=True, text=True, check=True
+            encoding='utf8', check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         shutil.copystat(src_file, interim_file)
     except subprocess.CalledProcessError as e:
-        raise RuntimeError('failed to compress "{0}": {1}'.format(src_file. str(e)))
+        raise RuntimeError('failed to compress "{}": {}'.format(src_file, str(e)))
 
-    dst_file = ''
-    if is_inplace:
-        dst_file = src_file
-    elif out_dir:
-        dst_file = prepare_dst_dir(path.abspath(out_dir), src_file)
-    else:
-        dst_file = prepare_dst_dir(path.join(path.dirname(src_file), SUFFIX), src_file)
-
-    print('TEST', interim_file, dst_file)
+    dst_file = prepare_dst_file(src_file, is_inplace, out_dir)
     os.rename(interim_file, dst_file)
+    return ImageInfo(src_file), ImageInfo(dst_file)
 
-def get_image_data(file_path):
+def get_image_size(file_path):
     try:
         proc = subprocess.run(
-            ['identify', '-format', r'%B %w %h %Q', file_path],
-            capture_output=True, text=True, check=True
+            ['identify', '-format', r'%w %h', file_path],
+            encoding='utf8', check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
-        size_str, w_str, h_str, quality_str = proc.stdout.split()
-        return [int(size_str), int(w_str), int(h_str), int(quality_str)]
+        w, h = map(int, proc.stdout.split())
+        return [w, h]
     except subprocess.CalledProcessError as e:
         raise RuntimeError(str(e))
 
@@ -50,21 +55,37 @@ def get_interim_file_path(file_path):
     name, ext = path.splitext(path.basename(file_path))
     return path.join(path.dirname(file_path), name + SUFFIX + ext)
 
-def prepare_dst_dir(dir_path, file_path):
+def prepare_dst_file(file_path, is_inplace, out_dir):
+    if is_inplace:
+        return file_path
+    dir_path = path.abspath(out_dir) if out_dir else path.join(path.dirname(file_path), SUFFIX)
     os.makedirs(dir_path, exist_ok=True)
     return path.join(dir_path, path.basename(file_path))
 
 def main():
     import sys
     import argparse
+
+    def check_quality_arg(val):
+        try:
+            quality = int(val)
+            if quality < 10 or quality > 99:
+                raise ValueError('value is out of range')
+        except Exception as e:
+            raise argparse.ArgumentTypeError(e)
+
     parser = argparse.ArgumentParser(description='Compresses images')
     parser.add_argument('targets', type=str, nargs='+')
-    parser.add_argument('--quality', type=int, default=DEFAULT_QUQLITY)
+    parser.add_argument('--quality', dest='quality', type=check_quality_arg, default=DEFAULT_QUALITY)
     parser.add_argument('--inplace', dest='is_inplace', action='store_true', default=False)
     parser.add_argument('--out-dir', dest='out_dir', type=str)
     args = parser.parse_args(sys.argv[1:])
     for target in args.targets:
-        compress_image(target, args.quality, args.is_inplace, args.out_dir)
+        try:
+            src, dst = compress_image(target, args.quality, args.is_inplace, args.out_dir)
+            print('{} -> {} ## {}'.format(src, dst, format(src.file_size / dst.file_size, '.2f')))
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     main()
