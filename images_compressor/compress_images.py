5081affe9cd3cc28ebfd95e78b21c746ecb764d5
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2021-09-05 20:59:57 +0300

    Image compressor (#1)
    * Put initial stubs
    
    * Add compression code
    
    * Refactor
    
    * Further refactor
    
    * Add size compression
    
    * Add some docs
    
    * Slightly refactor
    
    * Describe an issue
    
    * Fix 'invalid cross-device link' error
    
    * Add dockerfile
    
    * Make install script
    
    * Make separate folder

---
+++ images_compressor/compress_images.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+from os import path
+import os
+import subprocess
+import shutil
+
+SUFFIX = '.CMPRSIMG'
+
+class ImageInfo:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file_size = path.getsize(file_path)
+        w, h = get_image_size(file_path)
+        self.image_size = (w, h)
+
+    def __str__(self):
+        return '{} ({}K, {}x{})'.format(
+            self.file_path, round(self.file_size / 1024),
+            self.image_size[0], self.image_size[1]
+        )
+
+def compress_image(file_path, quality, max_size, is_inplace, out_dir):
+    src_file = path.abspath(file_path)
+    if not path.isfile(src_file):
+        raise RuntimeError('"{}" does not exist'.format(src_file))
+    src_info = ImageInfo(src_file)
+
+    # Interim file is used to preserve original file (for inplace case) so that
+    # its stats could then be copied.
+    interim_file = get_interim_file_path(src_file)
+    # There is an issue with `convert`.
+    # Since 6.9.10-23 (approximately) version `-quality` outputs significantly larger (~6x) file.
+    # This version is used in "focal" (u20) repositories.
+    args = ['convert']
+    if max_size is not None:
+        args.extend(['-resize', get_resize_param(src_info.image_size, max_size)])
+    if quality is not None:
+        args.extend(['-quality', str(quality) + '%'])
+    args.extend([src_file, interim_file])
+
+    subprocess.run(
+        args,
+        encoding='utf8', check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    shutil.copystat(src_file, interim_file)
+
+    dst_file = prepare_dst_file(src_file, is_inplace, out_dir)
+    # Using os.rename causes "Invalid cross-device link" error in docker.
+    shutil.move(interim_file, dst_file)
+    dst_info = ImageInfo(dst_file)
+    return src_info, dst_info
+
+def get_resize_param(image_size, max_size):
+    w_orig, h_orig = image_size
+    w_curr, h_curr = w_orig, h_orig
+    if w_orig > h_orig:
+        w_curr = max_size
+        h_curr = round(h_orig * w_curr / w_orig)
+    else:
+        h_curr = max_size
+        w_curr = round(w_orig * h_curr / h_orig)
+    return str(w_curr) + 'x' + str(h_curr) + '>'
+
+def get_image_size(file_path):
+    proc = subprocess.run(
+        ['identify', '-format', r'%w %h', file_path],
+        encoding='utf8', check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    w, h = map(int, proc.stdout.split())
+    return (w, h)
+
+def get_interim_file_path(file_path):
+    name, ext = path.splitext(path.basename(file_path))
+    return path.join(path.dirname(file_path), name + SUFFIX + ext)
+
+def prepare_dst_file(file_path, is_inplace, out_dir):
+    if is_inplace:
+        return file_path
+    dir_path = ''
+    if out_dir:
+        dir_path = path.abspath(out_dir)
+    else:
+        dir_path = path.join(path.dirname(file_path), SUFFIX)
+    os.makedirs(dir_path, exist_ok=True)
+    return path.join(dir_path, path.basename(file_path))
+
+def main():
+    import sys
+    import argparse
+
+    def check_quality_arg(val):
+        try:
+            quality = int(val)
+            if quality < 10 or quality > 100:
+                raise ValueError('value is out of range [10, 100]')
+            return quality
+        except Exception as e:
+            raise argparse.ArgumentTypeError(e)
+
+    def check_max_size_arg(val):
+        try:
+            max_size = int(val)
+            if max_size < 400 or max_size > 8000:
+                raise ValueError('value is out of range [400, 8000]')
+            return max_size
+        except Exception as e:
+            raise argparse.ArgumentTypeError(e)
+
+    parser = argparse.ArgumentParser(description='Compresses JPEG files')
+    parser.add_argument('targets', type=str, nargs='+', help='files to process')
+    parser.add_argument(
+        '--quality',
+        dest='quality', type=check_quality_arg,
+        help='compression quality / [10, 100]'
+    )
+    parser.add_argument(
+        '--max-size',
+        dest='max_size', type=check_max_size_arg,
+        help='max width/height (which is greater) / [400, 8000]'
+    )
+    parser.add_argument(
+        '--inplace',
+        dest='is_inplace', action='store_true', default=False,
+        help='replace file'
+    )
+    parser.add_argument(
+        '--out-dir',
+        dest='out_dir', type=str,
+        help='output directory'
+    )
+    args = parser.parse_args(sys.argv[1:])
+
+    for target in args.targets:
+        try:
+            src, dst = compress_image(
+                target,
+                args.quality, args.max_size, args.is_inplace, args.out_dir
+            )
+            print('{} -> {} // {}'.format(
+                src, dst, format(dst.file_size / src.file_size * 100, '.2f') + '%'
+            ))
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    main()
