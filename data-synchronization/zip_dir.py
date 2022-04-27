9c709671ad43a760b9864dc4a1dbd867279330bf
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-04-27 23:51:18 +0300

    Rework zip_dir

--- data-synchronization/zip_dir.py
+++ data-synchronization/zip_dir.py
@@ -4,40 +4,38 @@ from os import path
 import shutil
 import util
 
-def zip_directory(dir_path, archive_path):
-    archive_format = 'zip'
-    archive_name = dir_path
+def zip_directory(dir_path, archive_path, archive_format):
+    base_name = archive_path or dir_path
+    shutil.make_archive(base_name, archive_format, dir_path)
     if archive_path:
-        archive_name, archive_format = path.splitext(archive_path)
-        archive_format = archive_format[1:]
-    shutil.make_archive(archive_name, archive_format, dir_path)
+        shutil.move(base_name + '.' + archive_format, archive_path)
 
-def unzip_archive(archive_path, dir_path):
-    dir_name = path.splitext(archive_path)[0]
-    if dir_path:
-        dir_name = dir_path
-    shutil.unpack_archive(archive_path, dir_name)
+def unzip_archive(archive_path, dir_path, archive_format):
+    dir_name = dir_path or path.splitext(archive_path)[0]
+    if dir_name == archive_path:
+        dir_name += '_'
+    shutil.unpack_archive(archive_path, dir_name, archive_format)
 
-def zip_dir(target_path, output_path):
+def zip_dir(target_path, output_path, archive_format):
     full_target_path = path.abspath(target_path)
     full_output_path = output_path and path.abspath(output_path)
-    if path.isdir(full_target_path):
-        zip_directory(full_target_path, full_output_path)
-    elif path.isfile(full_target_path):
-        unzip_archive(full_target_path, full_output_path)
+    target_format = archive_format or 'zip'
+    if path.isfile(full_target_path):
+        unzip_archive(full_target_path, full_output_path, target_format)
     else:
-        raise RuntimeError('"{}" is neigher directory nor archive file'.format(full_target_path))
+        zip_directory(full_target_path, full_output_path, target_format)
 
 def main():
     def setup_args(parser):
         parser.add_argument('target', help='path to directory or archive')
+        parser.add_argument('--format', help='archive format')
         parser.add_argument('--output', help='path to archive or directory')
     args = util.parse_cmd_args(
         'Packs directory into archive or unpacks archive into directory.',
         setup_args
     )
 
-    zip_dir(args.target, args.output)
+    zip_dir(args.target, args.output, args.format)
 
 if __name__ == '__main__':
     main()
