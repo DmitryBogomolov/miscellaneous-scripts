5738fb1bc7eb0e0a2a6e39f25049fdfc2905437a
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2022-04-28 18:08:56 +0300

    Update zip (#8)
    * Rework zip_dir
    
    * Update util
    
    * Update
    
    * Rework encrypt_file
    
    * Add encrypt_dir script

--- data-synchronization/encrypt_file.py
+++ data-synchronization/encrypt_file.py
@@ -5,34 +5,37 @@ import util
 
 FILE_EXTENSION = '.pgp'
 
-def decrypt(file_path, output_path, is_dry_run):
+def is_encrypted_file(file_path):
+    return file_path.endswith(FILE_EXTENSION)
+
+def get_encrypted_name(name):
+    return name + FILE_EXTENSION
+
+def get_decrypted_name(name):
+    return name[:-len(FILE_EXTENSION)]
+
+def decrypt(file_path, output_path):
     args = ['gpg', '--decrypt']
-    if is_dry_run:
-        args.append('--dry-run')
-    # use output or strip encryption extension
-    output = output_path or file_path[:-len(FILE_EXTENSION)]
+    output = output_path or get_decrypted_name(file_path)
     args.extend(['--output', output, file_path])
     util.call_proc(args)
 
-def encrypt(file_path, output_path, user_id, is_dry_run):
+def encrypt(file_path, output_path, user_id):
     if not user_id:
-        raise RuntimeError('encryption recipient is not provided')
+        raise ValueError('encryption recipient is not provided')
     args = ['gpg', '--recipient', user_id, '--encrypt']
-    if is_dry_run:
-        args.append('--dry-run')
-    output = output_path or (file_path + FILE_EXTENSION)
+    output = output_path or get_encrypted_name(file_path)
     args.extend(['--output', output, file_path])
     util.call_proc(args)
 
-def encrypt_file(file_path, output_path, user_id, dry_run):
+def encrypt_file(file_path, output_path, user_id):
     full_file_path = path.abspath(file_path)
     full_output_path = output_path and path.abspath(output_path)
-    is_dry_run = util.is_dry_run(dry_run)
     util.check_file_exist(full_file_path)
-    if full_file_path.endswith(FILE_EXTENSION):
-        decrypt(full_file_path, full_output_path, is_dry_run)
+    if is_encrypted_file(full_file_path):
+        decrypt(full_file_path, full_output_path)
     else:
-        encrypt(full_file_path, full_output_path, user_id, is_dry_run)
+        encrypt(full_file_path, full_output_path, user_id)
 
 def main():
     def setup_args(parser):
@@ -41,11 +44,10 @@ def main():
         parser.add_argument('--user', help='user for encryption')
     args = util.parse_cmd_args(
         'Encrypts and decrypts files.',
-        setup_args,
-        add_dry_run=True
+        setup_args
     )
 
-    encrypt_file(args.file, args.output, args.user, args.dry_run)
+    encrypt_file(args.file, args.output, args.user)
 
 if __name__ == '__main__':
     main()
