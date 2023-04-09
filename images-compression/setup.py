c51b6807408b24c0646fd8f5c250bc2030a094e6
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-04-09 14:28:56 +0300

    Update setup

--- images-compression/setup.py
+++ images-compression/setup.py
@@ -8,17 +8,27 @@ import stat
 import shutil
 
 BIN_DIR = path.expanduser('~/.local/bin')
-TARGET = 'compress_images'
+SRC_DIR = path.dirname(path.abspath(__file__))
+TARGET_PY = 'compress_images'
+TARGET_SH = TARGET_PY + '_run'
 
-def install() -> None:
-    src_path = path.join(path.dirname(path.abspath(__file__)), TARGET + '.py')
-    dst_path = path.join(BIN_DIR, TARGET)
+def _install(target: str, ext: str) -> None:
+    src_path = path.join(SRC_DIR, target + ext)
+    dst_path = path.join(BIN_DIR, target)
     shutil.copy2(src_path, dst_path)
     os.chmod(dst_path, os.stat(dst_path).st_mode | stat.S_IEXEC)
 
-def uninstall() -> None:
-    file_path = path.join(BIN_DIR, TARGET)
+def _uninstall(target: str) -> None:
+    file_path = path.join(BIN_DIR, target)
     os.remove(file_path)
+
+def install() -> None:
+    _install(TARGET_PY, '.py')
+    _install(TARGET_SH, '.sh')
+
+def uninstall() -> None:
+    _uninstall(TARGET_PY)
+    _uninstall(TARGET_SH)
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Manages script')
